f3bde305e55a2a852ff004863f04dc5d56ccc2c9
Zainelabdeen S.A Elgraeed <zainco30@gmail.com>
2024-11-16 22:07:59 +0400

    updates

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -9,14 +9,12 @@ import {
 import { UsersService } from './users.service';
 import { User } from './user.schema';
 import { CreateUserDto } from './dto/create-user.dto';
-import { Public } from 'src/auth/decorators/public.decorator';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   // Create a new user
-  @Public()
   @Post()
   async createUser(@Body() createUserDto: CreateUserDto): Promise<User> {
     try {
