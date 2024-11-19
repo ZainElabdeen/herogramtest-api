8e20226c87e6147cddddca4c6a49597e17876a43
Zainelabdeen S.A Elgraeed <zainco30@gmail.com>
2024-11-19 12:00:18 +0400

    update main

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    cors: true,
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: ['http://localhost:3000', 'http://localhost:80'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
