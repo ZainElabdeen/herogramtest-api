925971a0b51fc4c3d25fcafbc5bb172bcae8121e
Zainelabdeen S.A Elgraeed <zainco30@gmail.com>
2024-11-19 13:34:37 +0400

    updated enableCors in main

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://localhost:80'],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    origin: 'http://64.225.101.136',
+    methods: 'GET, POST, PUT, DELETE, OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization',
+    credentials: true,
   });
-
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
